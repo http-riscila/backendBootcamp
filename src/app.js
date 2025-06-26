b5e7622032b230b65f02b5e677e8135aee0b7a5b
http-riscila <prigadelha10@gmail.com>
2025-06-25 22:32:24 -0300

    feat: service e controllers de item

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
+import itemsRouter from "./routes/item-routes.js";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api", itemsRouter);
 
 export default app;
