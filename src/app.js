1f601fb87ae5e3da402253fbb5da9352a5986169
http-riscila <prigadelha10@gmail.com>
2025-06-30 13:45:04 -0300

    fix: separacao entre controllers e services

--- src/app.js
+++ src/app.js
@@ -2,12 +2,15 @@ import express from "express";
 import itemsRouter from "./routes/item-routes.js";
 import proposalsRouter from "./routes/proposal-routes.js";
 import communityRoutes from "./routes/community-route.js";
+import membersRouter from "./routes/member-routes.js";
 
 const app = express();
 
 app.use(express.json());
+
 app.use("/api", itemsRouter);
 app.use("/api", proposalsRouter);
 app.use("/api", communityRoutes);
+app.use("/api", membersRouter);
 
 export default app;
