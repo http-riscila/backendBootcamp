af4c4040fc801bec2ae5bcd830da5775ada355ac
http-riscila <prigadelha10@gmail.com>
2025-06-30 18:08:39 -0300

    feat: validacoes para itens

--- src/routes/item-routes.js
+++ src/routes/item-routes.js
@@ -7,14 +7,35 @@ import {
   partiallyUpdateItem,
   removeItem,
 } from "../controllers/item-controllers.js";
+import handleValidationErrors from "../middlewares/validation.js";
+import {
+  createItemValidator,
+  updateItemValidator,
+  partiallyUpdateItemValidator,
+} from "../middlewares/item-validator.js";
 
 const itemsRouter = express.Router();
 
-itemsRouter.post("/items", createItem);
+itemsRouter.post(
+  "/items",
+  createItemValidator,
+  handleValidationErrors,
+  createItem
+);
 itemsRouter.get("/items", getAllItems);
 itemsRouter.get("/items/:id", getItemById);
-itemsRouter.put("/items/:id", updateItem);
-itemsRouter.patch("/items/:id", partiallyUpdateItem);
+itemsRouter.put(
+  "/items/:id",
+  updateItemValidator,
+  handleValidationErrors,
+  updateItem
+);
+itemsRouter.patch(
+  "/items/:id",
+  partiallyUpdateItemValidator,
+  handleValidationErrors,
+  partiallyUpdateItem
+);
 itemsRouter.delete("/items/:id", removeItem);
 
 export default itemsRouter;
