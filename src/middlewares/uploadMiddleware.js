637d2f2067812664443f6ca456d7349fcae332c7
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-07-03 20:30:07 -0300

    feat: adicionado middleware de upload para manipulação de arquivo de imagem com multer

---
+++ src/middlewares/uploadMiddleware.js
@@ -0,0 +1,32 @@
+import multer from "multer";
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, cb) =>{
+    if(file.mimetype.startsWith("image/")) {
+        cb(null, true);
+    } else {
+        cb(new Error("Invalid file. Only images are allowed. Please upload an image file."), false);
+    }
+}
+
+const limits = {
+    fileSize: 5 * 1024 * 1024 // Maximum file size of 5MB
+}
+
+const upload = multer({
+    storage,
+    fileFilter,
+    limits
+}).single("image");
+
+const uploadMiddleware = (req, res, next) => {
+    upload(req, res, (error) => {
+        if (error) {
+            return res.status(400).json({ message: error.message });
+        }
+        next();
+    });
+}
+
+export default uploadMiddleware;
