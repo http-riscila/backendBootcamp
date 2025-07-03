0f89f1ddca60a07c8064cc55d072071c6053b4be
Anderson Maicon Lemes De Souza Chaves <andersonchaves30@gmail.com>
2025-07-03 20:35:34 -0300

    feat: habilitado CORS no aplicativo para permitir requisições de diferentes origens

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import itemsRouter from "./routes/item-routes.js";
 import proposalsRouter from "./routes/proposal-routes.js";
 import communityRoutes from "./routes/community-route.js";
@@ -8,6 +9,7 @@ import authRouter from "./routes/auth-routes.js";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", itemsRouter);
